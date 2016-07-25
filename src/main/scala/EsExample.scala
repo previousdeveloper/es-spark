3c642763b03833d5bb252983eaacdacd018b44a1
gitphill <phillg82@gmail.com>
2016-07-25 21:03:53 +0100

    Add comments to EsExample

--- src/main/scala/EsExample.scala
+++ src/main/scala/EsExample.scala
@@ -3,15 +3,20 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.elasticsearch.spark._ 
 
+/**
+ * Simple es-hadoop example.
+ * Sends a map from spark to elasticsearch.
+ */
 object EsExample {
 
   def main(args: Array[String]) {
 
     val sparkConf = new SparkConf().setAppName("EsExample")
-    // sparkConf.set("es.nodes", "10.20.1.92")
     val sc = new SparkContext(sparkConf)
 
+    // create map
     val numbers = Map("one" -> 1, "two" -> 2, "three" -> 3)
+    // make RDD from map and save to elasticsearch
     sc.makeRDD(Seq(numbers)).saveToEs("spark/numbers")
   }
 }
